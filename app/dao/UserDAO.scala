c48b453f1229f9cdabce4614cee405b636fe41ad
gra-dev <sugorokuyarou@gmail.com>
2017-12-06 10:40:15 +0900

    update: UserDAOのメソッドで宣言を追加
    find,save,add,update,removeメソッドの宣言を追加

--- app/dao/UserDAO.scala
+++ app/dao/UserDAO.scala
@@ -1,7 +1,17 @@
 package dao
 
+import com.mohiva.play.silhouette.api.LoginInfo
+import model.user.RegisteredUser
+
+import scala.concurrent.Future
+
 trait UserDAO{
-  def save()
-  def find()
+  def find(loginInfo: LoginInfo): Future[Option[RegisteredUser]]
+  def find(userName: String): Future[Option[RegisteredUser]]
+  def save(user: RegisteredUser): Future[RegisteredUser]
+  def add(user: RegisteredUser): Future[RegisteredUser]
+  def update(user: RegisteredUser): Future[RegisteredUser]
+  def remove(loginInfo: LoginInfo): Future[Unit]
+  def remove(userName: String): Future[Unit]
 }
 
