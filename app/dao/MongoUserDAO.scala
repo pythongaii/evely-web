2b2b591a6bdea3a5f0fe4feda48bbb461bb55c06
gra-dev <sugorokuyarou@gmail.com>
2017-12-06 11:14:29 +0900

    update: MongoUserDAOにメソッド宣言を追加

--- app/dao/MongoUserDAO.scala
+++ app/dao/MongoUserDAO.scala
@@ -1,8 +1,16 @@
 package dao
+import com.mohiva.play.silhouette.api.LoginInfo
+import model.user.RegisteredUser
+
+import scala.concurrent.Future
 
 class MongoUserDAO extends UserDAO {
-  override def save(): Unit = ???
-
-  override def find(): Unit = ???
+  override def find(loginInfo: LoginInfo): Future[Option[RegisteredUser]] = ???
+  override def find(userName: String): Future[Option[RegisteredUser]] = ???
+  override def save(user: RegisteredUser): Future[RegisteredUser] = ???
+  override def add(user: RegisteredUser): Future[RegisteredUser] = ???
+  override def update(user: RegisteredUser): Future[RegisteredUser] = ???
+  override def remove(loginInfo: LoginInfo): Future[Unit] = ???
+  override def remove(userName: String): Future[Unit] = ???
 }
 
--- app/dao/SignUpTokenDAO.scala
+++ app/dao/SignUpTokenDAO.scala
@@ -7,9 +7,9 @@ import tokens.SignUpToken
 import scala.concurrent.Future
 
 trait SignUpTokenDAO {
-  def find(tokenId: UUID): Future[Option[SignUpToken]] = ???
-  def save(token: SignUpToken): Future[SignUpToken] = ???
-  def add(token: SignUpToken): Future[SignUpToken] = ???
-  def update(token: SignUpToken): Future[SignUpToken] = ???
-  def remove(token: SignUpToken): Future[SignUpToken] = ???
+  def find(tokenId: UUID): Future[Option[SignUpToken]]
+  def save(token: SignUpToken): Future[SignUpToken]
+  def add(token: SignUpToken): Future[SignUpToken]
+  def update(token: SignUpToken): Future[SignUpToken]
+  def remove(token: SignUpToken): Future[SignUpToken]
 }
