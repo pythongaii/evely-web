1eb17def50852f99c5c2d04be7fcdf8043370067
gra-dev <sugorokuyarou@gmail.com>
2017-12-06 12:26:55 +0900

    update: PasswordInfoServiceにメソッド宣言を追加

--- app/service/PasswordInfoService.scala
+++ app/service/PasswordInfoService.scala
@@ -1,20 +1,24 @@
 package service
 
 import java.util.concurrent.Future
+import javax.inject.Inject
 
 import com.mohiva.play.silhouette.api.LoginInfo
 import com.mohiva.play.silhouette.api.util.PasswordInfo
+import play.modules.reactivemongo.ReactiveMongoApi
 
 
-class PasswordInfoService{
+class PasswordInfoService @Inject() (reactiveMongoApi: ReactiveMongoApi) {
 
   def find(loginInfo: LoginInfo): Future[Option[PasswordInfo]] = ???
 
   def save(loginInfo: LoginInfo,
-           passwordInfo: PasswordInfo): Future[Option[PasswordInfo]] = ???
+           passwordInfo: PasswordInfo): Future[PasswordInfo] = ???
+
+  def add(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = ???
 
   def update(loginInfo: LoginInfo,
-             PasswordInfo: PasswordInfo): Future[Option[PasswordInfo]] = ???
+             PasswordInfo: PasswordInfo): Future[PasswordInfo] = ???
 
-  def delete(loginInfo: LoginInfo): Future[Unit] = ???
+  def remove(loginInfo: LoginInfo): Future[Unit] = ???
 }
