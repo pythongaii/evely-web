4180eea6e0ba086fb6354dab5c8d2ec3955df897
gra-dev <sugorokuyarou@gmail.com>
2017-12-06 15:40:26 +0900

    update: MongoSignUpToken,MongoUerDAOのメソッドを実装しました

--- app/dao/MongoSignUpTokenDAO.scala
+++ app/dao/MongoSignUpTokenDAO.scala
@@ -2,6 +2,7 @@ package dao
 import java.util.UUID
 import javax.inject.Inject
 
+import play.api.libs.json.Json
 import play.modules.reactivemongo.ReactiveMongoApi
 import reactivemongo.play.json.collection.JSONCollection
 import tokens.SignUpToken
@@ -10,11 +11,64 @@ import scala.concurrent.Future
 
 class MongoSignUpTokenDAO @Inject()(reactiveMongoApi: ReactiveMongoApi) extends SignUpTokenDAO {
 
-  val collection = reactiveMongoApi.database.map(_.collection[JSONCollection])
+  val signupTokenCollection = reactiveMongoApi.database.map(_.collection[JSONCollection]("signup_tokens"))
 
-  override def find(tokenId: UUID): Future[Option[SignUpToken]] = ???
-  override def save(token: SignUpToken): Future[SignUpToken] = ???
-  override def add(token: SignUpToken): Future[SignUpToken] = ???
-  override def update(token: SignUpToken): Future[SignUpToken] = ???
-  override def remove(tokenId: UUID): Future[Unit] = ???
+  /**
+    * DataBaseに保存されているSignUpTokenを取得する
+    * @param tokenId 検索に必要なキー
+    * @return
+    */
+  override def find(tokenId: UUID): Future[Option[SignUpToken]] = for {
+    tokens <- signupTokenCollection
+    signupToken <- tokens.find(Json.obj(
+      "tokenID" -> tokenId
+    )).one[SignUpToken]
+  } yield signupToken
+
+  /**
+    * 引数で渡されたトークンを保存する
+    * すでにトークンが保存されているなら update
+    * トークンの新規作成なら add を呼び出す
+    * @param token 保存対象のトークン
+    * @return 保存されたトークン
+    */
+  override def save(token: SignUpToken): Future[SignUpToken] = {
+    find(token.tokenID).flatMap{
+      case None => add(token)
+      case Some(signUpToken) => update(token)
+    }
+  }
+
+  /**
+    * トークンを新規追加する
+    * @param token 新規追加するトークン
+    * @return 追加されたトークン
+    */
+  override def add(token: SignUpToken): Future[SignUpToken] = for {
+    tokens <- signupTokenCollection
+    signupToken <- tokens.insert(token).map(_ => token)
+  } yield signupToken
+
+  /**
+    * 変更のあったトークンの更新
+    * @param token 更新するトークン
+    * @return 更新されたトークン
+    */
+  override def update(token: SignUpToken): Future[SignUpToken] = for {
+    tokens <- signupTokenCollection
+    _ <- tokens.update(Json.obj(
+      "tokenID" -> token.tokenID
+    ), Json.obj("$set" -> Json.obj("$" -> token)))
+    signupToken <- find(token.tokenID)
+  } yield signupToken.get
+
+  /**
+    * 登録したトークンの削除を行う
+    * @param tokenId 削除されるトークンのID
+    * @return Unitなので戻り値はない
+    */
+  override def remove(tokenId: UUID): Future[Unit] = for {
+    tokens <- signupTokenCollection
+    signupToken <- tokens.remove(Json.obj("tokenID" -> tokenId))
+  } yield signupToken
 }
--- app/dao/MongoUserDAO.scala
+++ app/dao/MongoUserDAO.scala
@@ -1,16 +1,95 @@
 package dao
+
+import javax.inject.Inject
+
 import com.mohiva.play.silhouette.api.LoginInfo
 import model.user.RegisteredUser
+import play.api.libs.json.Json
+import play.modules.reactivemongo.ReactiveMongoApi
+import reactivemongo.play.json.collection.JSONCollection
 
 import scala.concurrent.Future
 
-class MongoUserDAO extends UserDAO {
-  override def find(loginInfo: LoginInfo): Future[Option[RegisteredUser]] = ???
-  override def find(userName: String): Future[Option[RegisteredUser]] = ???
-  override def save(user: RegisteredUser): Future[RegisteredUser] = ???
-  override def add(user: RegisteredUser): Future[RegisteredUser] = ???
-  override def update(user: RegisteredUser): Future[RegisteredUser] = ???
-  override def remove(loginInfo: LoginInfo): Future[Unit] = ???
-  override def remove(userName: String): Future[Unit] = ???
+class MongoUserDAO @Inject()(reactiveMongoApi: ReactiveMongoApi) extends UserDAO {
+
+  val userCollection = reactiveMongoApi.database.map(_.collection[JSONCollection]("users"))
+
+  /**
+    * loginInfo を元にユーザ情報をDBから取得する
+    *
+    * @param loginInfo ユーザを一意に識別する
+    * @return 当該のユーザ情報 戻り値がからの場合もある
+    */
+  override def find(loginInfo: LoginInfo): Future[Option[RegisteredUser]] = for {
+    users <- userCollection
+    registerdUser <- users.find(Json.obj("loginInfo" -> loginInfo)).one[RegisteredUser]
+  } yield registerdUser
+
+  /**
+    * userName を元にユーザ情報をDBから取得する
+    *
+    * @param userName ユーザを一意に識別する
+    * @return 当該のユーザ情報 戻り値が空の場合もある
+    */
+  override def find(userName: String): Future[Option[RegisteredUser]] = for {
+    users <- userCollection
+    registerdUser <- users.find(Json.obj("userName" -> userName)).one[RegisteredUser]
+  } yield registerdUser
+
+  /**
+    * 引数で渡されたRegisterUserオブジェクトを保存する
+    * すでに保存されたユーザであれば、updateメソッド
+    * 新規登録ユーザであれば addメソッド
+    *
+    * @param user 登録するユーザオブジェクト
+    * @return 保存したユーザオブジェクト
+    */
+  override def save(user: RegisteredUser): Future[RegisteredUser] = {
+    find(user.userName).flatMap {
+      case None => add(user)
+      case Some(signUpToken) => update(user)
+    }
+  }
+
+  /**
+    * 引数で渡されたユーザ情報をDBに新規追加する
+    *
+    * @param user 新規保存するユーザ情報
+    * @return 保存したユーザ情報
+    */
+  override def add(user: RegisteredUser): Future[RegisteredUser] = for {
+    users <- userCollection
+    registeredUser <- users.insert(user).map(_ => user)
+  } yield registeredUser
+
+  /**
+    * 引数で渡されたユーザ情報で保存された情報を更新する
+    *
+    * @param user 更新があったユーザ情報
+    * @return 更新したユーザ情報
+    */
+  override def update(user: RegisteredUser): Future[RegisteredUser] = for {
+    users <- userCollection
+    _ <- users.update(Json.obj(
+      "userName" -> user.userName
+    ), Json.obj("$set" -> Json.obj("$" -> user)))
+    registeredUser <- find(user.userName)
+  } yield registeredUser.get
+
+  /**
+    * 登録したユーザ情報を削除する
+    *
+    * @param loginInfo ユーザを一意に識別する情報
+    * @return Unitなので戻り値はない
+    */
+  override def remove(loginInfo: LoginInfo): Future[Unit] = for {
+    users <- userCollection
+    user <- users.remove(Json.obj("loginInfo" -> loginInfo))
+  } yield user
+
+  override def remove(userName: String): Future[Unit] = for {
+    users <- userCollection
+    user <- users.remove(Json.obj("userName" -> userName))
+  } yield user
 }
 
