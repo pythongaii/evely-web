5d7d4623dcde68abe0e875696952c1dc1f7edfb8
gra-dev <sugorokuyarou@gmail.com>
2017-12-06 13:58:45 +0900

    update: MongoSignUpTokenDAOにreactiveMongoのModuleを追加

--- app/dao/MongoSignUpTokenDAO.scala
+++ app/dao/MongoSignUpTokenDAO.scala
@@ -1,11 +1,17 @@
 package dao
 import java.util.UUID
+import javax.inject.Inject
 
+import play.modules.reactivemongo.ReactiveMongoApi
+import reactivemongo.play.json.collection.JSONCollection
 import tokens.SignUpToken
 
 import scala.concurrent.Future
 
-class MongoSignUpTokenDAO extends SignUpTokenDAO {
+class MongoSignUpTokenDAO @Inject()(reactiveMongoApi: ReactiveMongoApi) extends SignUpTokenDAO {
+
+  val collection = reactiveMongoApi.database.map(_.collection[JSONCollection])
+
   override def find(tokenId: UUID): Future[Option[SignUpToken]] = ???
   override def save(token: SignUpToken): Future[SignUpToken] = ???
   override def add(token: SignUpToken): Future[SignUpToken] = ???
