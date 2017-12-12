00a06b9b5e3b002d30be0d1ad19e27f0a780a0b4
gra-dev <sugorokuyarou@gmail.com>
2017-12-12 14:15:42 +0900

    update: RegisteredHomeControllerにルーティング処理実装

--- app/controllers/RegisteredHomeController.scala
+++ app/controllers/RegisteredHomeController.scala
@@ -1,5 +1,33 @@
 package controllers
 
-class RegisteredHomeController {
+import javax.inject.Inject
+
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+
+import scala.concurrent.Future
+
+class RegisteredHomeController @Inject()()(implicit val messagesApi: MessagesApi) extends Controller with I18nSupport{
+
+
+  def home = Action.async { implicit request =>
+    Future.successful(Ok(views.html.test.home())
+  }
+
+  def index = Action.async { implicit request =>
+    Future.successful(Ok(views.html.test.index()))
+  }
+
+  def bookmark = Action.async { implicit request =>
+    Future.successful(Ok(views.html.test.bookmark()))
+  }
+
+  def notification = Action.async { implicit request =>
+    Future.successful(Ok(views.html.test.notification()))
+  }
+
+  def nearBy = Action.async { implicit request =>
+    Future.successful(Ok(views.html.test.nearby()))
+  }
 
 }
