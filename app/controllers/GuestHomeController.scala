9c233979333ed4f20796415f41be70f5bd522806
gra-dev <sugorokuyarou@gmail.com>
2018-01-21 16:16:07 +0900

    deploy

--- app/controllers/GuestHomeController.scala
+++ app/controllers/GuestHomeController.scala
@@ -10,7 +10,7 @@ import scala.concurrent.Future
 
 class GuestHomeController @Inject()(ws: WSClient)(implicit val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
-  def index() = Action.async {
+  def index() = Action.async { implicit request =>
     Future.successful(Ok(views.html.top(List("event1", "event2", "event3", "event4"))))
   }
 
